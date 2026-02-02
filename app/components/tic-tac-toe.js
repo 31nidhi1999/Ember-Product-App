4d8b21bcdd14a50952b4157c7e353fdfbb25fe21
Nidhi Nayak <nidhinayak31@gmail.com>
2026-02-02 11:50:19 +0530

    Added winner logic

--- app/components/tic-tac-toe.js
+++ app/components/tic-tac-toe.js
@@ -5,6 +5,16 @@ import { action } from "@ember/object";
 export default class TicTacToeComponent extends Component {
   @tracked board = Array(9).fill(null);
 
+  checkWinner(board){
+    const lines = [[0,1,2],[3,4,5,],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    for (let[a,b,c] of board){
+      if(board[a] === board[b] && board[b]===board[c] && board[c] === board[a]){
+        return board[a];
+      }
+      return null;
+    }
+  }
+
   @action
   reset() {
     this.board = Array(9).fill(null);
