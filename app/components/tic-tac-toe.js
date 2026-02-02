c0f792ddcc4d1378420e8c549e37599f83dd0e63
Nidhi Nayak <nidhinayak31@gmail.com>
2026-02-02 16:08:09 +0530

    winner logic modify

--- app/components/tic-tac-toe.js
+++ app/components/tic-tac-toe.js
@@ -13,13 +13,13 @@ export default class TicTacToeComponent extends Component {
   }
 
   checkWinner(board){
-    const lines = [[0,1,2],[3,4,5,],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    const lines = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     for (let [a,b,c] of lines){
       if(board[a] === board[b] && board[b]===board[c] && board[c] === board[a]){
         return board[a];
       }
-      return null;
     }
+    return null;
   }
 
   @action
