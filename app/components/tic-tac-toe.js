b7e491c6c361ba1efbd781647a67623cb5fceec8
Nidhi Nayak <nidhinayak31@gmail.com>
2026-02-02 12:02:25 +0530

    current player logic

--- app/components/tic-tac-toe.js
+++ app/components/tic-tac-toe.js
@@ -4,6 +4,11 @@ import { action } from "@ember/object";
 
 export default class TicTacToeComponent extends Component {
   @tracked board = Array(9).fill(null);
+  @tracked current = 'X';
+
+  get currentPlayer(){
+    return this.current;
+  }
 
   checkWinner(board){
     const lines = [[0,1,2],[3,4,5,],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
